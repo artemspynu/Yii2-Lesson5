4999e39e2e7cec6cac249a349804735efc12cff6
artem spynu <artem-lider@mail.ru>
2018-10-11 03:09:17 +0300

    Дз 5

---
+++ basic/models/User.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "user".
+ *
+ * @property int $id
+ * @property string $username
+ * @property string $name
+ * @property string $password_hash
+ * @property string $access_token
+ * @property string $auth_key
+ * @property int $creator_id
+ * @property int $updater_id
+ * @property int $creator_at
+ * @property int $updater_at
+ *
+ * @property Task[] $tasksCreated
+ * @property Task[] $tasksUpdated
+ * @property TaskUser[] $taskUsers
+ */
+class User extends \yii\db\ActiveRecord
+{
+    const RELATION_TASK_CREATED = 'tasksCreated';
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'user';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['username', 'name', 'password_hash', 'creator_id', 'creator_at'], 'required'],
+            [['creator_id', 'updater_id', 'creator_at', 'updater_at'], 'integer'],
+            [['username', 'name', 'password_hash', 'access_token', 'auth_key'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'username' => 'Username',
+            'name' => 'Name',
+            'password_hash' => 'Password Hash',
+            'access_token' => 'Access Token',
+            'auth_key' => 'Auth Key',
+            'creator_id' => 'Creator ID',
+            'updater_id' => 'Updater ID',
+            'creator_at' => 'Creator At',
+            'updater_at' => 'Updater At',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTasksCreated()
+    {
+        return $this->hasMany(Task::className(), ['creator_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTasksUpdated()
+    {
+        return $this->hasMany(Task::className(), ['updater_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTaskUsers()
+    {
+        return $this->hasMany(TaskUser::className(), ['user_id' => 'id']);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return \app\models\query\UserQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new \app\models\query\UserQuery(get_called_class());
+    }
+}
